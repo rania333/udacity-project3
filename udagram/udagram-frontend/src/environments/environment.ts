200626ae4c23e011fc41c3f3ad357174ac13c4a9
rania333 <raniaaramadan1999@gmail.com>
2022-12-14 00:55:24 +0200

    update url

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udacitybackend-env.eba-sxyvzg5k.us-east-1.elasticbeanstalk.com',
 };
 
 /*
